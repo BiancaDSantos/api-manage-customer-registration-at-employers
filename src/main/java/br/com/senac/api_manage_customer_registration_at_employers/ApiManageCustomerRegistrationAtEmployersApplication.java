6605b243c57de86ab5b7b1754576ae7c56d083dd
BiancaDSantos <biancadossantossc@gmail.com>
2025-03-04 14:34:55 -0300

    Initialization of the project in the GitHub code versioning tool.

---
+++ src/main/java/br/com/senac/api_manage_customer_registration_at_employers/ApiManageCustomerRegistrationAtEmployersApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.api_manage_customer_registration_at_employers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManageCustomerRegistrationAtEmployersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManageCustomerRegistrationAtEmployersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senac/api_manage_customer_registration_at_employers/ApiManageCustomerRegistrationAtEmployersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.api_manage_customer_registration_at_employers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiManageCustomerRegistrationAtEmployersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
